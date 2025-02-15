5d689db824dd9c1bf76cb9138bf407713c1d1405
Nooria <samihamaysoonnooria@gmail.com>
2025-02-15 21:27:42 +0600

    bvcxdcv g

--- polygon.py
+++ polygon.py
@@ -4,12 +4,21 @@ turtle.Screen().setup(300,400)
 
 polygon = turtle.Turtle()
 
-num_sides = 6
+num_sides = 4
 side_length = 70
 angle = 360.0 / num_sides
 
-for i in range(num_sides):
-    polygon.forward(side_length)
-    polygon.right(angle)
+
+polygon.forward(side_length)
+polygon.right(angle)
+
+polygon.forward(side_length)
+polygon.right(angle)
+
+polygon.forward(side_length)
+polygon.right(angle)
+
+polygon.forward(side_length)
+polygon.right(angle)
     
 turtle.done( )
